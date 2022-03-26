0f07a71a2b090524488d882cd1d9c50e9076ce30
WayneST3 <luck99bahek@mail.ru>
2022-03-26 17:50:37 +0300

    Project created. Added properties for db connection.

---
+++ src/test/java/com/search_system/SearchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.search_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
